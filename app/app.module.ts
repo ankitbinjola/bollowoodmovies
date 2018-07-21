cc52cda5e6058043b4bec59b53a6014011b8d4ec
Ankit Binjola <abinjola.binjola@gmail.com>
2018-07-21 07:43:54 +0530

    added_3(angular files)
    added two angular files

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import {NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import{Appcomponent} from './app.component';
+
+@NgModule({
+imports: [
+BrowserModule
+],
+declarations: [
+AppComponet
+],
+bootstrap: [
+AppComponent
+]
+})
+export class AppModule {}
